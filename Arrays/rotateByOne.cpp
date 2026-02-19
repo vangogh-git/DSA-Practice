4daf07b5d3507a2f92da9c21043fdcee6579871d
vangogh-git <vanisingh.work@gmail.com>
2026-02-19 19:34:33 +0530

    Added Rotate the Array by one

---
+++ Arrays/rotateByOne.cpp
@@ -0,0 +1,20 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// User function Template for C++
+
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int temp = arr[n-1];
+        
+        for(int i=n-1 ; i>=0 ; i--) {
+            arr[i] = arr[i-1];
+        }
+        
+        arr[0] = temp;
+        return ;
+    }
+};
