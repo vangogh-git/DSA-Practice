0f3f78c09defd8cad1e727a41b1244ce9a53cfcd
Vani Singh <vanisingh.work@gmail.com>
2026-03-01 22:55:48 +0530

    Linear Search

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,19 @@
+// Time Complexity : O(n)
+// Space Complexity: O(1)
+
+
+class Solution {
+  public:
+    int search(vector<int>& arr, int x) {
+        // code here
+        int n = arr.size();
+        
+        for(int i=0 ; i<n ; i++) {
+            if(arr[i] == x) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
