26b700b9d3cc25984435357762cb091690d79c31
Vani Singh <vanisingh.work@gmail.com>
2026-04-02 11:15:54 +0530

    Alternate Positive Negative

---
+++ Arrays/Alternate_positive_negative..cpp
@@ -0,0 +1,42 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+
+class Solution {
+  public:
+    void rearrange(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        vector<int> pos;
+        vector<int> neg;
+        
+        for(int i=0 ; i<n ; i++) {
+            if(arr[i] >= 0 ) {
+                pos.push_back(arr[i]);
+            }
+            else{
+                neg.push_back(arr[i]);
+            }
+        }
+        
+        // now insert into arr the alternative way
+        int i=0 , j=0 , k=0;
+        int p = pos.size();
+        int m = neg.size();
+        
+        while(i < p && j < m) {
+            arr[k++] = pos[i++];
+            arr[k++] = neg[j++];
+        }
+        
+        while(i < p) {
+            arr[k++] = pos[i++];
+        }
+        
+        while(j < m) {
+            arr[k++] = neg[j++];
+        }
+        return ;
+    }
+};
