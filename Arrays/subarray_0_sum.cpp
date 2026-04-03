607a680dc70dff82e0add74b93fbb8534f135c65
Vani Singh <vanisingh.work@gmail.com>
2026-04-03 22:02:05 +0530

    Subarray with 0 sum

---
+++ Arrays/subarray_0_sum.cpp
@@ -0,0 +1,24 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
+class Solution {
+  public:
+    
+    bool subArrayExists(vector<int>& arr) {
+       
+        
+        unordered_map<int , bool> m;
+        
+        int sum = 0;
+        for(int i=0 ; i<arr.size() ; i++) {
+            sum += arr[i];
+            if(sum == 0 || m[sum] == true){
+                return true;
+            }
+            m[sum] = true;
+        }
+        
+        return false;
+    }
+};
