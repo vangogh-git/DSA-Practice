506ef0ff14f6224fe83feb3827c2671e9ff03ccf
Vani Singh <vanisingh.work@gmail.com>
2026-04-16 11:02:29 +0530

    More than n/k Occurrences
    Implemented a function to count elements in an array that appear more than n/k times using an unordered map.

--- Arrays/element_appreaing_more_than_nktimes.cpp
+++ Arrays/element_appreaing_more_than_nktimes.cpp
@@ -1,1 +1,35 @@
+-// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+class Solution {
+  public:
+    // Function to find all elements in array that appear more than n/k times.
+    int countOccurence(vector<int>& arr, int k) {
+        
+        // create a temp array for mapping using unordered maps
+        unordered_map<int, int> m;
+        int n = arr.size();
+        
+        int check = n / k;
+        
+        int count = 0;
+        
+        for(int i=0 ; i<n ; i++) {
+            int index = arr[i];
+            if(m[index] == -1){
+                continue;
+            }
+            m[index]++;
+            // check if the count in the map is greater than check value
+            if(m[index] > check) {
+                // mark as visited
+                m[index] = -1;
+                count++;
+            }
+        }
+        
+        return count++;
+        
+        
+    }
+};
