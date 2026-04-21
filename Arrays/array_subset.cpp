aa1368f2b53518d2532f601a9c16b05baa9d2cc5
Vani Singh <vanisingh.work@gmail.com>
2026-04-21 23:20:43 +0530

    find whether an array is a subset of another array
    Implemented a function to check if one vector is a subset of another using a hash map for efficient lookups.

---
+++ Arrays/array_subset.cpp
@@ -0,0 +1,32 @@
+// time complexity: O(n+m)
+// space complexity: O(n)
+
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        // Your code here
+        
+        unordered_map<int,int> mp;
+        int n = a.size();
+        int m = b.size();
+        
+        for(int i=0 ; i<n ; i++) {
+            mp[a[i]]++;
+        }
+        
+        // now check if b is there in map or not
+        for(int i=0 ; i<m ; i++) {
+            int element = b[i];
+            
+            if(mp[element] == 0) {
+                return false;
+            }
+            
+            mp[element]--;
+
+        }
+        
+        return true;
+    }
+};
