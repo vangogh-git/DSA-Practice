9e3626d0437adb0d3c153ae812172b122f144f35
Vani Singh <vanisingh.work@gmail.com>
2026-05-14 12:49:37 +0530

    Binary Search in a Matrix

---
+++ Matrix/Binary_search_matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int start = 0;
+        int end = (n * m)-1;
+
+        int mid = start + (end-start)/2;
+
+        while(start <= end) {
+            if(matrix[mid/m][mid%m] == target) {
+                return true;
+            }
+
+            else if(matrix[mid/m][mid%m] > target) {
+                end = mid - 1;
+            }
+            
+            else{
+                start = mid + 1;
+            }
+
+            mid = start + (end-start)/2;
+        }
+
+        return false;
+
+    }
+};
