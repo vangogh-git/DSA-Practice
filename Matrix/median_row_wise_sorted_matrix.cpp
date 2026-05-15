a7d65bcf8a144bd7c72012dff9575cd28be92177
Vani Singh <vanisingh.work@gmail.com>
2026-05-15 21:59:50 +0530

    Median in a row-wise sorted matrix

--- Matrix/median_row_wise_sorted_matrix.cpp
+++ Matrix/median_row_wise_sorted_matrix.cpp
@@ -33,4 +33,45 @@ class Solution {
 // Time Complexity: O(nlogm * log(maxVal - minVal))
 // Space Complexity: O(1)
 
+class Solution {
+  public:
+    int median(vector<vector<int>> &mat) {
+        
+        // find low high 
+        int low = INT_MAX;
+        int high = INT_MIN;
+        
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i=0 ; i<n ; i++) {
+            low = min(low , mat[i][0]);
+            high = max(high , mat[i][m-1]);
+        }
+        int ans = -1;
+        int req = (n*m+1)/2;
+        int mid = low + (high-low)/2;
+        // now apply binary search on matrix
+        while(low <= high) {
+            mid = low + (high-low)/2;
+            
+            int count = 0;
+            
+            for(int i=0 ; i<n ; i++) {
+                count += upper_bound(mat[i].begin() , mat[i].end() , mid) - mat[i].begin();
+            }
+            
+            if(count >= req){
+                ans = mid;
+                high = mid - 1;
+            }
+            
+            else{
+                low = mid + 1;
+            }   
+        }
+        return ans;
+    }
+};
+
 
