1057d3547d7b8c8e95d32067281729deac47d1af
Vani Singh <vanisingh.work@gmail.com>
2026-05-16 22:10:53 +0530

    Maximum Difference Between pair in a matrix

---
+++ Matrix/max_diff_btw_pair.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findMaxValue(vector<vector<int>>& mat, int N) {
+        
+        // first we will try to find a minimum value
+        // and everytime we also find a maximum value corresponding
+        // to that minimum value
+        // keeping the condition c > a and d > b in mind
+        
+        int mini = INT_MAX;
+        
+        for(int i=0 ; i< ; i++) {
+            for(int j=0 ; j< ; j++) {
+                
+            }
+        }
+        
+    }
+};
