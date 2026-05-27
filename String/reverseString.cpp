19b1aeacb655d389748954ee554c0ffb433b8758
Vani Singh <vanisingh.work@gmail.com>
2026-05-27 21:22:52 +0530

    Reverse a String
    Implemented a recursive method to reverse a string using a helper function.

--- String/reverseString.cpp
+++ String/reverseString.cpp
@@ -16,3 +16,30 @@ public:
         }
     }
 };
+
+// using recursion
+// time complexity : O(n)
+// space complexity: O(n) as recursion uses stack space
+
+class Solution {
+    private:
+    void Reverse(vector<char>& s , int i , int j) {
+        // base case
+        if(i > j) return ;
+
+        else {
+            swap(s[i] , s[j]);
+            i++;
+            j--;
+            Reverse(s , i , j);
+            return ;
+        }
+    }
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        Reverse(s , i , j);
+        return ;
+    }
+};
