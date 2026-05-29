2a2713f4b0849bf8312105a227450206eb6053e3
Vani Singh <vanisingh.work@gmail.com>
2026-05-29 11:04:17 +0530

    Minimum Number of Flips
    Refactor minFlips function to simplify counting flips for both starting patterns.

--- String/min_number_flips.cpp
+++ String/min_number_flips.cpp
@@ -1,9 +1,14 @@
+// Brute Force Solution
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 class Solution {
   public:
     int minFlips(string& s) {
-        
+        // code here
         string start_zero = "";
-        string start_one = ""; 
+        string start_one = "";
         
         for(int i=0 ; i<s.size() ; i++) {
             if(i % 2 == 0) {
@@ -11,32 +16,26 @@ class Solution {
                 start_one.push_back('1');
             }
             else{
+                //odd places 
                 start_zero.push_back('1');
                 start_one.push_back('0');
             }
         }
-        
-        // now compare the final strings with the original and
-        // return minimum count
-        int cnt = 0;
-        
-        for(int i=0 ; i<s.size() ; i++) {
-            if(s[i] == start_zero[i]) {
-                cnt++;
-            }
+    
+    int cnt_zero = 0;
+    int cnt_one = 0;
+    
+    for(int i=0 ; i<s.size() ; i++) {
+        if(s[i] == start_zero[i]) {
+            cnt_zero++;
         }
-        
-        int mini = cnt;
-        cnt = 0;
-        for(int i=0 ; i<s.size() ; i++) {
-            if(s[i] == start_one[i]) {
-                cnt++;
-            }
-            mini = min(mini , cnt);
+        if(s[i] == start_one[i]) {
+            cnt_one++;
         }
+    }
+    
+    return min(cnt_zero , cnt_one);
         
-        return mini;
-        
-          
     }
 };
+
