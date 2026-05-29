c3ff2ac351bc804a82dd342644fe65a4297abfb8
Vani Singh <vanisingh.work@gmail.com>
2026-05-29 12:53:02 +0530

    Roman to Integer

---
+++ String/roman_to_integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+        
+        unordered_map<char,int> value = {
+            {'I', 1} , {'V' , 5} , {'X' , 10} , {'L' , 50} ,
+            {'C', 100} , {'D' , 500} , {'M' , 1000}
+        };
+        
+        int ans = 0;
+        
+        for(int i=0 ; i<s.size() ; ) {
+            if( i+1 < s.size() &&
+            ((s.substr(i ,2) == "IV") || (s.substr(i ,2) == "IX") || (s.substr(i ,2) == "XL") || (s.substr(i ,2) == "XC") || (s.substr(i ,2) == "CD") || (s.substr(i ,2) == "CM")) ){
+                int temp = value[s[i+1]] - value[s[i]];
+                ans += temp;
+                i += 2;
+            }
+            else{
+                ans += value[s[i]];
+                i += 1;
+            }
+        }
+        
+        return ans;
+    }
+};
