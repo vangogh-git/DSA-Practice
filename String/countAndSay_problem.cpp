ced4e3f96e484980976475beb904e847bd292432
Vani Singh <vanisingh.work@gmail.com>
2026-06-04 12:05:48 +0530

    Count and Say Problem

---
+++ String/countAndSay_problem.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    private:
+    void solve(int n , string &s) {
+        //base case
+        if( n== 1){
+            s = "1";
+            return ;
+        }
+
+        solve(n-1 , s);
+        
+        
+        int cnt = 1;
+        for(int i=0 ; i<s.size() ; i++) {
+            char element = s[i];
+            if(i+1 < s.size() && s[i] == s[i+1] ) {
+                cnt++;
+            }
+            else if(i + 1 == s.size() || s[i] != s[i+1]) {
+                s.replace(i-cnt+1 , cnt , to_string(cnt) + element);
+                i = (i - cnt + 1) + 1;
+                cnt = 1;
+            }
+        }
+    }
+public:
+    string countAndSay(int n) {
+        string s = "";
+        solve(n , s);
+        return s;
+
+    }
+};
