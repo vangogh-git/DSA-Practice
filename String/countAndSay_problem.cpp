43c63efb4db046bb03fc907a83cbc5846450fa40
Vani Singh <vanisingh.work@gmail.com>
2026-06-04 14:28:09 +0530

    Update countAndSay_problem.cpp

--- String/countAndSay_problem.cpp
+++ String/countAndSay_problem.cpp
@@ -35,3 +35,42 @@ public:
 
     }
 };
+
+// Optimized Approach -> Using Append Function
+// Time Complexity : O(2^n)
+// Space Complexity: O(n) + O(2^n) = O(2^n)
+
+class Solution {
+    private: 
+    void solve(int n , string &s) {
+        if(n == 1) {
+            s = "1";
+            return ;
+        }
+
+        solve(n-1 , s);
+
+        string temp = "";
+        int cnt = 1;
+        for(int i=0 ; i<s.size() ; i++) {
+            char element = s[i];
+            if(i+1 < s.size() && s[i] == s[i+1]) {
+                cnt++;
+            }
+            else if(i == s.size()-1 || s[i] != s[i+1]) {
+                temp.append(to_string(cnt));
+                temp += element;
+                cnt = 1;
+            }
+        }
+        s = temp;
+
+    }
+public:
+    string countAndSay(int n) {
+        string s = "";
+        //string temp = "";
+        solve(n , s);
+        return s;
+    }
+};
