2a5cc193768f634246a788a940eb3535b7854967
Vani Singh <vanisingh.work@gmail.com>
2026-06-09 23:59:27 +0530

    Implement countMinReversals function for braces

---
+++ String/count_reversal_brace.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    int countMinReversals(string s) {
+        
+        // odd condition
+        if(s.size() & 1) {
+            return -1; // odd case
+        }
+        
+        // valid string removal
+        stack<char> st;
+        for(int i=0 ; i<s.size() ; i++) {
+            if(s[i] == '{') {
+                st.push(s[i]);
+            }
+            else {
+                // it is a closed brace
+                if(!st.empty() && st.top() == '{') {
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+        }
+        
+        // now find the invalid string answer
+        int a=0 , b = 0;
+        while(!st.empty()) {
+            if(st.top() == '{') {
+                a++;
+            }
+            else{
+                b++;
+            }
+            st.pop();
+        }
+        
+        return (a+1)/2 + (b+1)/2;
+    }
+};
