3848d5a170671095785325c29e83fcfdff9ff2ea
Vani Singh <vanisingh.work@gmail.com>
2026-06-14 21:08:38 +0530

    Update replace_consecutive_two_same_with_one.cpp

--- String/replace_consecutive_two_same_with_one.cpp
+++ String/replace_consecutive_two_same_with_one.cpp
@@ -18,3 +18,62 @@ class Solution {
         return ans;
     }
 };
+
+/////////////////////////
+// RECURSIVE SOLUTION
+
+class Solution {
+    private: 
+    void solve(string &s , string &ans , char prev , int index){
+        // base case
+        if(index >= s.size()) {
+            return ;
+        }
+        
+        if(s[index] != prev) {
+            ans += s[index];
+            prev = s[index];
+        }
+        
+        solve(s , ans , prev , index + 1);
+    }
+  public:
+    string removeDuplicates(string& s) {
+        int i = 0;
+        string ans = "";
+        char prev = '#';
+        solve(s , ans , prev , i);
+        return ans;
+    }
+};
+
+
+// STACK BASED APPROACH
+// T.C : O(n)
+// S.C : O(n)
+class Solution {
+  public:
+    string removeDuplicates(string& s) {
+        
+        stack<char> st;
+        
+        for(char ch : s) {
+            if(st.empty() || ch != st.top() ) {
+                st.push(ch);
+            }
+        }
+        
+        // now we have the required stack
+        // stack -> ans
+        string ans = "";
+        
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        
+        // we reverse ans and return it
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
