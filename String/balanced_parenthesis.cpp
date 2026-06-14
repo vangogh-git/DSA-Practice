57d5664f961144c2b0e378f83a32911d63c594c2
Vani Singh <vanisingh.work@gmail.com>
2026-06-14 12:38:22 +0530

    Create balanced_parenthesis.cpp

---
+++ String/balanced_parenthesis.cpp
@@ -0,0 +1,36 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        
+        stack<char> st;
+
+        for(int i=0 ; i<s.size() ; i++) {
+            char ch = s[i];
+            
+            if(ch == '(' || ch == '{' || ch == '[') {
+            st.push(ch);
+            }
+            else{
+                if(st.empty()) {
+                    return false;
+                }
+                
+                if((ch == ')' && st.top() == '(') || 
+                    (ch == '}' && st.top() == '{') ||
+                    (ch == ']' && st.top() == '[')) {
+                        st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return st.empty();
+        
+    }
+};
