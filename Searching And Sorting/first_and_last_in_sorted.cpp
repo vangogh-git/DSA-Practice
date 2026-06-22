2f70d20409852dc14714e3afb5cff6f208d5d1e5
Vani Singh <vanisingh.work@gmail.com>
2026-06-22 19:07:00 +0530

    Update first_and_last_in_sorted.cpp

--- Searching And Sorting/first_and_last_in_sorted.cpp
+++ Searching And Sorting/first_and_last_in_sorted.cpp
@@ -31,3 +31,66 @@ class Solution {
         
     }
 };
+
+// time complexity : O(log n)
+// space complexity : O(1)
+
+class Solution {
+    private: 
+    int find_first(vector<int>& nums, int target){
+        int first_index = INT_MAX;
+        int beg = 0;
+        int end = nums.size()-1;
+        bool found = false;
+        
+        while(beg <= end) {
+            int mid = beg + (end-beg)/2;
+            if(nums[mid] == target) {
+                    first_index = mid;
+                    found = true;
+                    end = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                beg = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        if(!found) return -1;
+        return first_index;
+    }
+    int find_last(vector<int>& nums, int target){
+    int last_index = INT_MIN;
+        int beg = 0;
+        int end = nums.size()-1;
+        bool found = false;
+    
+        while(beg <= end) {
+            int mid = beg + (end-beg)/2;
+            if(nums[mid] == target) {
+                    last_index = mid;
+                    found = true;
+                    beg = mid + 1;
+            }
+            else if(nums[mid] < target) {
+                beg = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        if(!found) return -1;
+        return last_index;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        if(nums.size() == 0) return {-1,-1};
+
+        ans.push_back(find_first(nums,target));
+        ans.push_back(find_last(nums,target));
+
+        return ans;
+    }
+};
