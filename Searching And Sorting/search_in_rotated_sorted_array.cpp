02dc330947c8e982df95cae70cf05b0356700480
Vani Singh <vanisingh.work@gmail.com>
2026-06-23 21:03:37 +0530

    Update search_in_rotated_sorted_array.cpp

--- Searching And Sorting/search_in_rotated_sorted_array.cpp
+++ Searching And Sorting/search_in_rotated_sorted_array.cpp
@@ -55,4 +55,33 @@ public:
 // time complexity : O(log n)
 // space complexity : O(1)
 
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int beg = 0;
+        int end = nums.size()-1;
+        while(beg <= end) {
+            int mid = beg + (end-beg)/2;
+            if(target == nums[mid]) {
+                return mid;
+            }
+            if(nums[beg] <= nums[mid] ) {
+                if(target >= nums[beg] && target <= nums[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    beg = mid + 1;
+                }
+            }
+            else {
+                if(target > nums[mid] && target <= nums[end]) {
+                    beg = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
