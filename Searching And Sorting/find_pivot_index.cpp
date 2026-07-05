556e5625b75df7ba70a7916196482066df70d3cf
Vani Singh <vanisingh.work@gmail.com>
2026-07-05 21:48:30 +0530

    Update find_pivot_index.cpp

--- Searching And Sorting/find_pivot_index.cpp
+++ Searching And Sorting/find_pivot_index.cpp
@@ -24,3 +24,23 @@ public:
         return -1;
     }
 };
+
+// time complexity : O(n)
+// space complexity : O(1)
+// More Cleaner Approach - better for interviews - avoids i+1 edge case
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int total = 0;
+        for(int num : nums)  total += num;
+        int left_sum = 0;
+        for(int i=0 ; i<n ; i++) {
+            int right_sum = total - left_sum - nums[i];
+            if(left_sum == right_sum ) return i;
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
