ae0bdf61bc694ea2715ac742c95d8cadcd5af104
Vani Singh <vanisingh.work@gmail.com>
2026-07-05 17:07:37 +0530

    Update find_pivot_index.cpp

--- Searching And Sorting/find_pivot_index.cpp
+++ Searching And Sorting/find_pivot_index.cpp
@@ -1,1 +1,26 @@
+// time complexity : O(n)
+// space complexity : O(1)
+// brute force approach : 
 
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left_sum = 0;
+        int right_sum = 0;
+        for(int i=1 ; i<nums.size() ; i++) {
+            right_sum += nums[i];
+        }
+        for(int i=0 ; i<nums.size() ; i++) {
+            if(left_sum == right_sum ) {
+                return i;
+            }
+            else {
+                left_sum += nums[i];
+                if(i < nums.size()-1) {
+                    right_sum -= nums[i+1];
+                }
+            }
+        }
+        return -1;
+    }
+};
