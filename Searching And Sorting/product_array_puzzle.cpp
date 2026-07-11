7c44dfe922c5cc0389c13bdd2ab8a05a2d054b99
Vani Singh <vanisingh.work@gmail.com>
2026-07-11 20:23:43 +0530

    Update product_array_puzzle.cpp
    Implemented an optimal solution for the product array puzzle using prefix and suffix products.

--- Searching And Sorting/product_array_puzzle.cpp
+++ Searching And Sorting/product_array_puzzle.cpp
@@ -22,3 +22,49 @@ class Solution {
         return ans;
     }
 };
+
+// time complexity : O(n)
+// space complexity : O(n)
+// better approach
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n) , suffix(n) , answer;
+
+        prefix[0] = nums[0];
+        for(int i=1 ; i<n ; i++) {
+            prefix[i] = prefix[i-1] * nums[i];
+        }
+
+        suffix[n-1] = nums[n-1];
+        for(int i=n-2 ; i>=0 ; i--) {
+            suffix[i] = suffix[i+1] * nums[i];
+        }
+
+        for(int i=0 ; i<n ; i++) {
+            if(i == 0) {
+                answer.push_back(suffix[i+1]);
+            }
+            else if(i == n-1) {
+                answer.push_back(prefix[i-1]);
+            }
+            else {
+                answer.push_back(suffix[i+1] * prefix[i-1]);
+            }
+        }
+
+        return answer;
+
+        
+    }
+};
+
+// time complexity : O(n)
+// space complexity : O(1)
+// optimal Approach -> interview level
+
+
+
+
