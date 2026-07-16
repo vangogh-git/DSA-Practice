e72503f98897928578ffd73aa4be4578671cb9c6
Vani Singh <vanisingh.work@gmail.com>
2026-07-16 21:11:11 +0530

    Update minimum_swaps_to_sort.cpp

--- Searching And Sorting/minimum_swaps_to_sort.cpp
+++ Searching And Sorting/minimum_swaps_to_sort.cpp
@@ -25,3 +25,33 @@ class Solution {
         return cnt;
     }
 };
+
+// Optimal Approach
+// Time Complexity : O(nlogn)
+// Space Complexity : O(n)
+
+
+class Solution {
+  public:
+    int minSwaps(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int,int>>v;
+        for(int i=0 ; i<n ; i++) {
+            v.push_back({arr[i],i});
+        }
+        sort(v.begin() , v.end());
+        int swap_cnt = 0;
+        for (int i=0 ; i<arr.size() ; i++) {
+            pair<int,int>p = v[i];
+            int index = p.second;
+            int value = p.first;
+            
+            if(i != index) {
+                swap(v[i] , v[index]);
+                swap_cnt++;
+                i--;
+            }
+        }
+        return swap_cnt;
+    }
+};
