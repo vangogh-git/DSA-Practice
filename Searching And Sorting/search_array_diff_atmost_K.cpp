ad21e5a3ea4e6172240ad6a1c2d732f875aae57d
Vani Singh <vanisingh.work@gmail.com>
2026-07-18 10:16:33 +0530

    Update search_array_diff_atmost_K.cpp

--- Searching And Sorting/search_array_diff_atmost_K.cpp
+++ Searching And Sorting/search_array_diff_atmost_K.cpp
@@ -15,3 +15,29 @@ class Solution {
         return -1;
     }
 };
+
+
+// optimized Solution 
+// here the time and space complexities is same but it is more optimized because
+// we are taking the advantage of step array and k value for jumping to only useful indexes and skipping unnecessary.
+
+class Solution {
+  public:
+    int findStepKeyIndex(vector<int>& arr, int k, int x) {
+        
+        // edge case when k = 0
+        if(k == 0) {
+            for(int i=0 ; i<arr.size(); i++) {
+                if(arr[i] == x) return i;
+            }
+        }
+
+        for(int i=0 ; i<arr.size() ; ) {
+            if(arr[i] == x) return i;
+            
+             int jump = max (1 , abs(arr[i]-x)/k);
+             i += jump;
+        }
+        return -1;
+    }
+};
