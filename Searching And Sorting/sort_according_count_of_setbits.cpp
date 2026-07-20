69b6d76ab8a3fbc62b3c272a9b1a1371b231fb8c
Vani Singh <vanisingh.work@gmail.com>
2026-07-20 09:16:41 +0530

    Update sort_according_count_of_setbits.cpp

--- Searching And Sorting/sort_according_count_of_setbits.cpp
+++ Searching And Sorting/sort_according_count_of_setbits.cpp
@@ -32,3 +32,52 @@ class Solution {
         return ans;
     }
 };
+
+// time complexity : O(nlogn)
+// space complexity : O(1)
+// Note : this is the actual solution for this problem because it doesn't provide stable sort but indeed it is a clever approach.
+class Solution {
+    private:
+    int countOfBits(int num) {
+        int cnt = 0;
+        while(num) {
+            cnt += (num & 1);
+            num >>= 1;
+        }
+        return cnt;
+    }
+  public:
+    vector<int> sortBySetBitCount(vector<int>& arr) {
+        int n = arr.size();
+        int M = *max_element(arr.begin() , arr.end()) + 1;
+        
+        for(int i=0 ; i<n ; i++) {
+            int bits = countOfBits(arr[i]);
+            arr[i] += bits * M;
+        }
+        
+        // sort
+        sort(arr.begin(), arr.end(), greater<int>());
+        
+        // extract back
+        for(int i=0 ; i<n ; i++) {
+            arr[i] = arr[i] % M;
+        }
+        return arr;
+    }
+};
+
+// optimized Solution
+// time complexity: O(nlogn)
+// space complexity: O(1)
+
+class Solution {
+  public:
+    vector<int> sortBySetBitCount(vector<int>& arr) {
+        
+        stable_sort(arr.begin() , arr.end() , [](int a , int b){
+           return __builtin_popcount(a) > __builtin_popcount(b); 
+        });
+        return arr;
+    }
+};
