8b6b3bf923322a8cbffbde6d8d07cbc4f966cb5d
Vani Singh <vanisingh.work@gmail.com>
2026-07-21 22:02:48 +0530

    Update inplace_merge_sort.cpp

--- Searching And Sorting/inplace_merge_sort.cpp
+++ Searching And Sorting/inplace_merge_sort.cpp
@@ -1,1 +1,33 @@
+// time complexity : O(m+n)
+// space complexity : O(1)
+// optimized Solution
 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = n+m;
+
+        for(int i=0 ; i<n ; i++) {
+            nums1[m+i] = nums2[i];
+        }
+
+        // apply gap method
+        int gap = (len/2) + len%2;
+
+        while(gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while(right < len) {
+                if(nums1[left] > nums1[right]) {
+                    swap(nums1[left] , nums1[right]);
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) break;
+            gap = (gap/2) + (gap%2);
+        }
+        return ;
+    }
+};
