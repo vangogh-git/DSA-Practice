c73d8022080b331386c2b9d39b424250742e6cd6
Vani Singh <vanisingh.work@gmail.com>
2026-08-01 10:07:56 +0530

    Create bishu_and_soldiers.cpp

---
+++ Searching And Sorting/bishu_and_soldiers.cpp
@@ -0,0 +1,61 @@
+// time complexity : O(NlogN) + O(QlogN)
+// space complexity : O(N)
+// pattern : binary search + prefix sum
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int findCount(vector<int> &arr , int power) {
+    int ans = -1;
+    int low = 0;
+    int high = arr.size()-1;
+    while(low <= high) {
+        int mid = low + (high - low)/2;
+        if(arr[mid] <= power) {
+            ans = mid;
+            low = mid+1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans+1;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+    for(int i=0 ; i<N ; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin() , arr.end());
+
+    // create prefix sum array
+    vector<int> prefix(N);
+    prefix[0] = arr[0];
+    for(int i=1; i<N ; i++) {
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+
+    int Q;
+    cin >> Q;
+    while(Q--) {
+        int power;
+        cin >> power;
+
+        int cnt = findCount(arr,power);
+        // check the cnt
+        int sum = 0;
+        if(cnt > 0) {
+            sum = prefix[cnt-1];
+        }
+
+        cout << cnt << " " << sum << endl;
+    }
+
+    return 0;
+}
