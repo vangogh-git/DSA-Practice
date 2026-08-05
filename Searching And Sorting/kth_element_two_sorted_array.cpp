44a5f20a14ccd60713ee664c1f5f01e929968862
Vani Singh <vanisingh.work@gmail.com>
2026-08-05 08:30:02 +0530

    Update kth_element_two_sorted_array.cpp

--- Searching And Sorting/kth_element_two_sorted_array.cpp
+++ Searching And Sorting/kth_element_two_sorted_array.cpp
@@ -1,3 +1,49 @@
+// optimized solution
+// time complexity : O(log(min(n,m)))
+// space complexity : O(1)
+
+
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        
+        int n = a.size();
+        int m = b.size();
+        
+        if(n > m) {
+            return kthElement(b , a , k);
+        } 
+        
+        int low = max(0,k-m) , high = min(k , n);
+        
+        
+        while(low <= high) {
+            int cut1 = (low + high) >> 1;
+            int cut2 = k - cut1;
+            
+            int l1 = cut1 == 0 ? INT_MIN : a[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : b[cut2-1];
+            int r1 = cut1 == n ? INT_MAX : a[cut1];
+            int r2 = cut2 == m ? INT_MAX : b[cut2];
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1,l2);
+            }
+            
+            else if(l1 > r2) {
+                high = cut1 - 1;
+            }
+            
+            else {
+                low = cut1 + 1;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
 // time complexity : O(n+m log n+m)
 // space complexity : O(1)
 
