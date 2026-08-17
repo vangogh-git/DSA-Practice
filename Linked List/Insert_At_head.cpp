dbb2115f16388263eef3df6152036b06f3d5243e
Vani Singh <vanisingh.work@gmail.com>
2026-08-17 08:32:29 +0530

    Create Insert_At_head.cpp

---
+++ Linked List/Insert_At_head.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node* next = NULL;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void InsertAtHead(Node* &head , int d) {
+    // we insert
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void printNodes(Node* &head) {
+    
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+int main() {
+    
+    // we create the node first
+    Node* node1 = new Node(10);
+
+    Node* head = node1;
+
+    InsertAtHead(head , 12);
+
+    printNodes(head);
+}
