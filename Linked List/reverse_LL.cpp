2b02938486a862766ecf8dbbf838af45931c109e
Vani Singh <vanisingh.work@gmail.com>
2026-08-19 08:31:36 +0530

    Update reverse_LL.cpp

--- Linked List/reverse_LL.cpp
+++ Linked List/reverse_LL.cpp
@@ -34,3 +34,53 @@ class Solution {
         
     }
 };
+
+/* Structure of Linked List Node
+class Node {
+ public:
+    int data ;
+    Node *next ;
+
+    Node(int x) {
+        data = x ;
+        next = nullptr ;
+    }
+};
+*/
+
+// time complexity  : O(n)
+// space complexity : O(n) // recursion call stack storage
+// recursive approach
+
+class Solution {
+    private: 
+    Node* solve(Node* prev , Node* curr) {
+        // first we create base case
+        if(curr == nullptr) {
+            return prev;
+        }
+        else {
+            Node* temp = curr -> next;
+            curr -> next = prev;
+            return solve(curr , temp);
+        }
+    }
+  public:
+    Node* reverseList(Node* head) {
+        
+        // iterative 
+        Node* prev = NULL;
+        Node* curr = head;
+        // while(curr != NULL) {
+        //     next = curr -> next;
+        //     curr -> next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+        
+        return solve(prev , curr);
+        
+        //return prev;
+        
+    }
+};
