46796b53439ce169941087a6ff6658b866f831bc
Vani Singh <vanisingh.work@gmail.com>
2026-08-23 12:43:34 +0530

    Update middle_element_in_LL.cpp

--- Linked List/middle_element_in_LL.cpp
+++ Linked List/middle_element_in_LL.cpp
@@ -46,3 +46,37 @@ public:
         return curr;
     }
 };
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Using Tortoise and Hare Approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+        if(head == NULL) return NULL;
+        if(head -> next == NULL) return head;
+
+        // using fast and slow approach
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL) {
+            if(fast -> next == NULL) return slow;
+            slow = slow -> next;
+            fast = (fast -> next) -> next;
+        }
+
+        return slow;
+    }
+};
