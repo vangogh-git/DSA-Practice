81fe96a8eb20a3921c71bb7354221f60bc3f33cf
Vani Singh <vanisingh.work@gmail.com>
2026-08-23 19:31:21 +0530

    Update kth_from_end_node.cpp

--- Linked List/kth_from_end_node.cpp
+++ Linked List/kth_from_end_node.cpp
@@ -1,5 +1,6 @@
 // Time Complexity : O(n)
 // Space Complexity : O(1)
+// Brute Force Approach
 
 /* Structure of Linked List Node
 class Node {
@@ -36,5 +37,45 @@ class Solution {
             cnt++;
         }
         return temp->data;
+    }
+};
+
+// Optimal Solution
+// Time Complexity : O(n)
+// Space Complexity: O(1)
+
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    int getKthFromLast(Node* head, int k) {
+        
+        if(head == NULL) return NULL;
+        if(head -> next == NULL && k != 1) return -1;
+        
+        Node* curr = head;
+        Node* prev = head;
+        int cnt = 1;
+        while(cnt <= k) {
+            if(curr == NULL) return -1;
+            curr = curr -> next;
+            cnt++;
+        }
+        
+        while(curr != NULL) {
+            prev = prev -> next;
+            curr = curr -> next;
+        }
+        return prev->data;
+        
     }
 };
