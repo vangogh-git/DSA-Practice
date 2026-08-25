ef606ecdc02bb3e10bb75dcd39fef17bbcbbcf61
Vani Singh <vanisingh.work@gmail.com>
2026-08-25 21:36:53 +0530

    Update segregate_even_odd_value_nodes.cpp

--- Linked List/segregate_even_odd_value_nodes.cpp
+++ Linked List/segregate_even_odd_value_nodes.cpp
@@ -47,3 +47,50 @@ class Solution {
         
     }
 };
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Optimized Solution
+
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+}; */
+class Solution {
+  public:
+    Node* divide(Node* head) {
+        
+        Node* odd = new Node(-1);
+        Node* even = new Node(-1);
+        
+        Node* oddhead = odd;
+        Node* evenhead = even;
+        
+        Node* pointer = head;
+        while(pointer != NULL) {
+            if(pointer->data % 2 != 0) {
+                odd -> next = pointer;
+                odd = odd -> next;
+            }
+            else {
+                even -> next = pointer;
+                even = even -> next;
+            }
+            pointer = pointer -> next;
+        }
+        
+        even -> next = NULL;
+        odd -> next = NULL;
+        
+        even -> next = oddhead -> next;
+        return evenhead -> next;
+        
+    }
+};
