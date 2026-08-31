77243cd9452f4e115794a78b05268ceb45465654
Vani Singh <vanisingh.work@gmail.com>
2026-08-31 19:34:21 +0530

    Create Add_one_to_LL.cpp

---
+++ Linked List/Add_one_to_LL.cpp
@@ -0,0 +1,65 @@
+// time complexity: O(n)
+// space complexity : O(1)
+
+
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+    private:
+    void reverse(Node* &head) {
+        Node* prev = NULL;
+        Node* curr = head;
+        
+        while(curr != NULL) {
+            Node* next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        head = prev;
+    }
+  public:
+    Node* addOne(Node* head) {
+        
+        // first we reverse the LL
+        reverse(head);
+        
+        // now we apply the Add +1 logic
+        Node* curr = head;
+        
+        while(curr != NULL) {
+            if(curr -> data != 9) {
+                curr -> data += 1;
+                break;
+            }
+            else {
+                // equal to 9
+                curr -> data = 0;
+                if(curr -> next == NULL) {
+                    Node* node1 = new Node(1);
+                    curr -> next = node1;
+                    break;
+                }
+                else {
+                   curr = curr -> next; 
+                }
+            }
+        }
+        
+        // reverse again
+        reverse(head);
+        return head;
+        
+    }
+};
