fd79ccd0533db4efbec59cbaf7b69c09ffee0991
Vani Singh <vanisingh.work@gmail.com>
2026-09-01 12:20:07 +0530

    Update add_numbers_LL.cpp

--- Linked List/add_numbers_LL.cpp
+++ Linked List/add_numbers_LL.cpp
@@ -1,3 +1,7 @@
+// time complexity : O(n+m)
+// space complexity : O(1)
+// Optimal in-place LinkedList Modification Solution
+
 /*
 class Node {
   public:
@@ -28,6 +32,16 @@ class Solution {
     
   public:
     Node* addTwoLists(Node* head1, Node* head2) {
+        if(head1 -> next == NULL && head1 -> data == 0) return head2;
+        if(head2 -> next == NULL && head2 -> data == 0) return head1;
+
+        while(head1->data == 0 && head1->next != NULL) {
+            head1 = head1 -> next;
+        }
+        
+        while(head2->data == 0 && head2->next != NULL) {
+            head2 = head2 -> next;
+        }
         
         reverse(head1);
         reverse(head2);
@@ -38,51 +52,44 @@ class Solution {
         
         int carry = 0;
         while(curr1 != NULL && curr2 != NULL) {
-            int temp = curr1->data + curr2->data + carry;
-            curr1->data = temp % 10;
-            carry = temp / 10;
-            
-            if(curr1 -> next == NULL && curr2 -> next == NULL && carry != 0) {
-                Node* node1 = new Node(carry);
-                curr1 -> next = node1;
-            }
+            int sum = curr1 -> data + curr2 -> data + carry;
+            curr1 -> data = sum % 10;
+            carry = sum / 10;
             
             prev1 = curr1;
             curr1 = curr1 -> next;
             curr2 = curr2 -> next;
-        }
+        } 
         
         while(curr1 != NULL) {
-            int temp = curr1->data + carry;
-            curr1 -> data = temp % 10;
-            carry = temp / 10;
+            int sum = curr1 -> data + carry;
+            curr1 -> data = sum % 10;
+            carry = sum / 10;
             
-            if(curr1 -> next == NULL && carry != 0) {
-                Node* node1 = new Node(carry);
-                curr1 -> next = node1;
-            }
-            
+            prev1 = curr1;
             curr1 = curr1 -> next;
         }
         
         while(curr2 != NULL) {
-            int temp = curr2 -> data + carry;
-            prev1 -> next = new Node(temp % 10);
-            carry = temp / 10;
-            
-            if(curr2 -> next == NULL && carry != 0) {
-                prev1 -> next = new Node(carry);
-            }
+            int sum = curr2 -> data + carry;
+            prev1 -> next = new Node(sum % 10);
+            carry = sum / 10;
             
             prev1 = prev1 -> next;
             curr2 = curr2 -> next;
         }
         
+        if(carry != 0) {
+            prev1 -> next = new Node(carry);
+        }
+        
         reverse(head1);
         
-        while(head1 -> data == 0) {
+        // remove starting zeros
+        while(head1->data == 0 && head1->next != NULL) {
             head1 = head1 -> next;
         }
+        
         return head1;
     }
 };
