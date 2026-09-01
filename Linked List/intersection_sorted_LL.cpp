1464ba0750bcbac7884dabf556a56767df21bfc2
Vani Singh <vanisingh.work@gmail.com>
2026-09-01 20:18:52 +0530

    Update intersection_sorted_LL.cpp

--- Linked List/intersection_sorted_LL.cpp
+++ Linked List/intersection_sorted_LL.cpp
@@ -63,3 +63,62 @@ class Solution {
         
     }
 };
+
+
+// t.c : O(n+m)
+// s.c : O(1)
+// clean code + optimized Solution
+
+
+/* Structure of a Linked list Node 
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        
+        if(head1 == NULL || head2 == NULL) return NULL;
+        
+        Node* c1 = head1;
+        Node* c2 = head2;
+        Node* head = NULL;
+        Node* tail = NULL;
+        
+        while(c1 != NULL && c2 != NULL) {
+            if(c1 -> data == c2 -> data) {
+                if(head == NULL) {
+                    head = c1;
+                    tail = c1;
+                }
+                else {
+                    tail -> next = c1;
+                    tail = c1;
+                }
+                
+                c1 = c1 -> next;
+                c2 = c2 -> next;
+            }
+            else if(c1 -> data < c2 -> data) {
+                c1 = c1 -> next;
+            }
+            else {
+                c2 = c2 -> next;
+            }
+        }
+        
+        if(tail != NULL) {
+            tail -> next = NULL;
+        }
+        
+        return head;        
+    }
+};
