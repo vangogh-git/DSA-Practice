f5d533de92c976605596f423591b32754fd98836
Vani Singh <vanisingh.work@gmail.com>
2026-09-03 14:53:44 +0530

    Update intersection_in_Y_shaped_list.cpp

--- Linked List/intersection_in_Y_shaped_list.cpp
+++ Linked List/intersection_in_Y_shaped_list.cpp
@@ -1,5 +1,5 @@
 // time complexity : O(n+m)
-// space complexity : O(n+m)
+// space complexity : O(n)
 // brute force Approach --- Using hashmap
 
 
@@ -36,6 +36,74 @@ class Solution {
             }
             c2 = c2 -> next;
         }
+        return NULL;
+    }
+};
+
+// time complexity: O(n+m)
+// space complexity : O(1)
+// Optimized Approach
+
+/* Structure of Linked List Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        
+        if(head1 == NULL || head2 == NULL) NULL;
+        
+        Node* t1 = head1;
+        Node* t2 = head2;
+        int n1 = 0;
+        int n2 = 0;
+        
+        while(t1 != NULL) {
+            t1 = t1 -> next;
+            n1++;
+        }
+        
+        while(t2 != NULL) {
+            t2 = t2 -> next;
+            n2++;
+        }
+        
+        t1 = head1;
+        t2 = head2;
+        
+        int d = abs(n1 - n2);
+        
+        if(n1 > n2) {
+            while(d != 0) {
+                t1 = t1 -> next;
+                d--;
+            }
+        }
+        else {
+            while(d != 0) {
+                t2 = t2 -> next;
+                d--;
+            }
+        }
+        
+        while(t1 != NULL && t2 != NULL) {
+            if(t1 == t2) {
+                return t1;
+            }
+            
+            t1 = t1 -> next;
+            t2 = t2 -> next;
+        }
+        
         return NULL;
     }
 };
