05e067d7cb818883ec7d65f30a360ca500aa3446
Vani Singh <vanisingh.work@gmail.com>
2026-09-08 12:46:56 +0530

    Update sort_0s_1s_2s.cpp

--- Linked List/sort_0s_1s_2s.cpp
+++ Linked List/sort_0s_1s_2s.cpp
@@ -1,3 +1,8 @@
+// brute force approach --- will hit TLE
+// time complexity : O(2n)
+// space complexity : O(1)
+
+
 /* Node is defined as
   class Node {
   public:
@@ -14,79 +19,38 @@ class Solution {
   public:
     Node* segregate(Node* head) {
         
-        Node* head0 = NULL;
-        Node* head1 = NULL;
-        Node* head2 = NULL;
+        int cnt1 = 0 , cnt2 = 0 , cnt0 = 0;
         Node* temp = head;
-        bool found0 = false , found1 = false , found2 = false;
         while(temp != NULL) {
-            if(temp -> data == 1 && !found1) {
-                head1 = temp;
-                found1 = true;
+            if(temp -> data == 0) {
+                cnt0++;
             }
-            else if(temp -> data == 2 && !found2) {
-                head2 = temp;
-                found2 = true;
+            else if(temp -> data == 1) {
+                cnt1++;
             }
-            else if(temp -> data == 0 && !found0) {
-                head0 = temp;
-                found0 = true;
+            else {
+                cnt2++;
+            }
+        }
+        
+        temp = head;
+        while(temp != nullptr) {
+            if(cnt0 != 0) {
+                temp -> data = 0;
+                cnt0--;
+            }
+            else if(cnt1 != 0) {
+                temp -> data = 1;
+                cnt1--;
+            }
+            else {
+                temp -> data = 2;
+                cnt2--;
             }
             temp = temp -> next;
         }
         
-        Node* prev = head1;
-        Node* curr = head1 -> next;
-        
-        while(curr != NULL) {
-            if(curr -> data == 1) {
-                prev -> next = curr;
-                prev = curr;
-                curr = curr -> next;
-            }
-            else {
-                curr = curr -> next;
-            }
-        }
-        if(curr == NULL) prev -> next = NULL;
-        Node* tail1 = prev;
-        
-        prev = head2;
-        curr = head2 -> next;
-        
-        while(curr != NULL) {
-            if(curr -> data == 2) {
-                prev -> next = curr;
-                prev = curr;
-                curr = curr -> next;
-            }
-            else {
-                curr = curr -> next;
-            }
-        }
-        if(curr == NULL) prev -> next = NULL;
-        Node* tail2 = prev;
-        
-        prev = head0;
-        curr = head0 -> next;
-        
-        while(curr != NULL) {
-            if(curr -> data == 0) {
-                prev -> next = curr;
-                prev = curr;
-                curr = curr -> next;
-            }
-            else {
-                curr = curr -> next;
-            }
-        }
-        if(curr == NULL) prev -> next = NULL;
-        Node* tail0 = prev;
-        
-        tail0 -> next = head1;
-        tail1 -> next = head2;
-        return head0;
-        
+        return head;
         
     }
 };
