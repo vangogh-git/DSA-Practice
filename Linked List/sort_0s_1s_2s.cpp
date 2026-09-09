56c8fc5f9ccdadf026aff3c06504f51e76675f64
Vani Singh <vanisingh.work@gmail.com>
2026-09-09 09:06:40 +0530

    Update sort_0s_1s_2s.cpp

--- Linked List/sort_0s_1s_2s.cpp
+++ Linked List/sort_0s_1s_2s.cpp
@@ -54,3 +54,60 @@ class Solution {
         
     }
 };
+
+// time complexity : O(n)
+// space complexity : O(1)
+// optimized approach
+
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        
+        if(head == nullptr || head -> next == nullptr) {
+            return head;
+        } 
+        
+        Node* zerohead = new Node(-1);
+        Node* zero = zerohead;
+        Node* onehead = new Node(-1);
+        Node* one = onehead;
+        Node* twohead = new Node(-1);
+        Node* two = twohead;
+        
+        Node* temp = head;
+        while(temp != NULL) {
+            if(temp -> data == 1){
+                one -> next = temp;
+                one = one -> next;
+                
+            }
+            if(temp -> data == 2){
+                two -> next = temp;
+                two = two -> next;
+            } 
+            if(temp -> data == 0){
+                zero -> next = temp;
+                zero = zero -> next;
+            }
+            temp = temp -> next;
+        }
+        
+        zero -> next = (onehead -> next) ? onehead -> next : twohead -> next;
+        one -> next = twohead -> next;
+        two -> next = NULL;
+        
+        return zerohead->next;
+    }
+};
