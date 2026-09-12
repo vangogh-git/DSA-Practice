932756bcf3e1f08d657571acb987169916e40e1a
Vani Singh <vanisingh.work@gmail.com>
2026-09-12 09:32:37 +0530

    Update merge_two_sorted_LL.cpp

--- Linked List/merge_two_sorted_LL.cpp
+++ Linked List/merge_two_sorted_LL.cpp
@@ -53,3 +53,60 @@ class Solution {
         return head1;
     }
 };
+
+// time complexity : O(n+m)
+// space complexity:  O(1)
+// Optimized Approach
+
+
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        
+        
+        Node* newNode = new Node(-1);
+        Node* temp = newNode;
+        
+        Node* temp1 = head1;
+        Node* temp2 = head2;
+        while(temp1 != NULL && temp2 != NULL) {
+            if(temp1 -> data <= temp2 -> data){
+                temp -> next = temp1;
+                temp1 = temp1 -> next;
+            } 
+            else {
+                temp -> next = temp2;
+                temp2 = temp2 -> next;
+            }
+            temp = temp -> next;
+        }
+        
+        while(temp1 != NULL) {
+            temp -> next = temp1;
+            temp = temp -> next;
+            temp1 = temp1 -> next;
+        }
+        
+        while(temp2 != NULL) {
+            temp -> next = temp2;
+            temp = temp -> next;
+            temp2 = temp2 -> next;
+        }
+        
+        return newNode -> next;
+        
+    }
+};
