238b3d797a68ce116f02454b18f01b331358ff01
Vani Singh <vanisingh.work@gmail.com>
2026-09-19 09:58:09 +0530

    Update pair_sum_in_sorted_DLL.cpp

--- Linked List/pair_sum_in_sorted_DLL.cpp
+++ Linked List/pair_sum_in_sorted_DLL.cpp
@@ -54,3 +54,54 @@ class Solution {
         
     }
 };
+
+// time complexity : O(n)
+// space complexity : O(1)
+// Optimized Approach
+
+/* Structure of Doubly Linked List Node
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    vector<vector<int>> givenSumPairs(Node* head, int target) {
+       
+        vector<vector<int>> output;
+        Node* tail = head;
+        while(tail -> next != NULL) {
+            tail = tail -> next;
+        }
+        
+        Node* left = head;
+        Node* right = tail;
+        
+        while(left != right && left->prev != right) {
+            
+            if(left->data + right -> data == target) {
+                output.push_back({left->data,right->data});
+                left = left -> next;
+                right = right -> prev;
+            }
+            else if(left->data + right -> data > target) {
+                right = right -> prev;
+            }
+            else {
+                left = left -> next;
+            }
+        }
+        
+        return output;
+        
+    }
+};
